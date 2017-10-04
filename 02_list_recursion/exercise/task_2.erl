8ea59abeb1193bf645f2190dd080184bf19649f1
safebuster <safebuster2@gmail.com>
2017-10-04 16:24:27 +0300

    ok

--- 02_list_recursion/exercise/task_2.erl
+++ 02_list_recursion/exercise/task_2.erl
@@ -7,29 +7,37 @@
 
 %% implement erlang:length/1
 %% http://www.erlang.org/doc/man/erlang.html#length-1
-len(List) ->
-    0.
+len(List) -> len(List, 0).
+len(List, Acc) ->
+  case List of
+    [] -> Acc;
+    [_ | Tail] -> len(Tail, Acc + 1)
+  end.
 
 
 len_test() ->
-    ?assertEqual(0, len([])),
-    ?assertEqual(1, len([1])),
-    ?assertEqual(2, len([1,2])),
-    ?assertEqual(5, len([1,2,3,4,5])),
-    ?assertEqual(10, len([1,1,1,1,1,2,2,2,2,2])),
-    ok.
+  ?assertEqual(0, len([])),
+  ?assertEqual(1, len([1])),
+  ?assertEqual(2, len([1, 2])),
+  ?assertEqual(5, len([1, 2, 3, 4, 5])),
+  ?assertEqual(10, len([1, 1, 1, 1, 1, 2, 2, 2, 2, 2])),
+  ok.
 
 
 %% implement lists:reverse/1
 %% http://www.erlang.org/doc/man/lists.html#reverse-1
-reverse(List) ->
-    List.
+reverse(List) -> reverse(List, []).
+reverse(List, Rev) ->
+  case List of
+    [] -> Rev;
+    [Head | Tail] -> reverse(Tail, [Head | Rev])
+  end.
 
 
 reverse_test() ->
-    ?assertEqual([], reverse([])),
-    ?assertEqual([1], reverse([1])),
-    ?assertEqual([2,1], reverse([1,2])),
-    ?assertEqual([5,4,3,2,1], reverse([1,2,3,4,5])),
-    ?assertEqual(["ef", "cd", "ab"], reverse(["ab", "cd", "ef"])),
-    ok.
+  ?assertEqual([], reverse([])),
+  ?assertEqual([1], reverse([1])),
+  ?assertEqual([2, 1], reverse([1, 2])),
+  ?assertEqual([5, 4, 3, 2, 1], reverse([1, 2, 3, 4, 5])),
+  ?assertEqual(["ef", "cd", "ab"], reverse(["ab", "cd", "ef"])),
+  ok.
