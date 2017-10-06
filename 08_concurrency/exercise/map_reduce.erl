f92e21a4ae6c3e5a1dd336720eb86ed46725851e
safebuster <safebuster2@gmail.com>
2017-10-06 15:16:43 +0300

    ok

--- 08_concurrency/exercise/map_reduce.erl
+++ 08_concurrency/exercise/map_reduce.erl
@@ -4,4 +4,22 @@
 
 
 start(Files) ->
-    maps:new().
+  %Self = self(),
+  %Pids = [spawn(fun() -> Self ! readlines(File) end) || File <- Files],
+  Words = lists:foldl(fun(File, Acc) -> lists:append(Acc, readlines(File)) end, [], Files),
+  lists:foldl(fun(Word, Map) ->
+    BinWord = binary:list_to_bin(Word),
+    case maps:find(BinWord, Map) of
+      {ok, Cnt} -> maps:put(BinWord, Cnt + 1, Map);
+      error -> maps:put(BinWord, 1, Map)
+    end
+              end
+    , maps:new()
+    , Words
+  ).
+
+readlines(FileName) ->
+  case file:read_file(FileName) of
+    {ok, Data} -> re:split(binary:bin_to_list(Data), "[\t\s\n\r]", [{return, list}]);
+    {error, _} -> []
+  end.
