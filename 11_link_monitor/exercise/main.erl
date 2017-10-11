5d30eac9c6b11d16b3b94bd21c57ddb7b0fbc441
cobr123 <safebuster2@gmail.com>
2017-10-11 17:58:54 +0300

    ok

--- 11_link_monitor/exercise/main.erl
+++ 11_link_monitor/exercise/main.erl
@@ -4,4 +4,34 @@
 
 
 parse(Files) ->
-    ok.
+  process_flag(trap_exit, true),
+  Self = self(),
+  FileRefFromPid = lists:foldl(fun(File, Acc) ->
+    Ref = make_ref(),
+    Param = {File, Ref, spawn_link(worker, get_data, [Ref, Self, File])},
+    [Param | Acc] end
+    , []
+    , Files
+  ),
+  lists:foldl(fun({File, Ref, FromPid}, {ResAcc, ErrAcc}) ->
+    receive
+      {'EXIT', FromPid, normal} -> {ResAcc, ErrAcc};
+      {'EXIT', FromPid, Reason} -> {ResAcc, maps:put(File, Reason, ErrAcc)};
+      {result, Ref, List} -> {add_to_res(List, ResAcc), ErrAcc}
+    end
+              end
+    , {maps:new(), maps:new()}
+    , FileRefFromPid
+  ).
+
+
+add_to_res(List, Map) ->
+  lists:foldl(fun({K, V}, Acc) ->
+    case maps:find(K, Acc) of
+      {ok, Value} -> maps:put(K, V + Value, Acc);
+      error -> maps:put(K, V, Acc)
+    end
+              end
+    , Map
+    , List
+  ).
--- 11_link_monitor/exercise/worker.erl
+++ 11_link_monitor/exercise/worker.erl
@@ -1,1 +1,24 @@
 -module(worker).
+
+-export([get_data/3]).
+
+
+get_data(Ref, FromPid, FileName) ->
+  Lines = readlines(FileName),
+  Vals = lists:foldl(fun(Line, Acc) -> [parse_line(Line) | Acc] end
+    , []
+    , Lines
+  ),
+  FromPid ! {result, Ref, Vals},
+  ok.
+
+
+parse_line(Line) ->
+  [_, BinName, BinCnt, _] = binary:split(Line, [<<",">>], [global]),
+  Cnt = binary_to_integer(BinCnt),
+  {BinName, Cnt}.
+
+
+readlines(FileName) ->
+  {ok, Data} = file:read_file(FileName),
+  binary:split(Data, [<<"\r">>, <<"\n">>], [global, trim_all]).
